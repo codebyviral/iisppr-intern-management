970381b66007c00a3d7efdec43e6d49cb34ac552
Viral Vaghela <viralvaghela8577@gmail.com>
2024-12-14 19:28:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/pageIndex";
-import Sidebar from "./components/Sidebar";
-import "./Sidebar.css"; // Import the sidebar styling
 
 function App() {
   return (
-    <div style={{ display: "flex" }}>
-      {/* Sidebar Component */}
-      <Sidebar />
 
-      {/* Main Content */}
-      <div style={{ flex: 1, padding: "20px" }}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
-      </div>
-    </div>
   );
 }
 
