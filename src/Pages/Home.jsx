89e9a49ddcbf7d6d362715a5c006a039b4766b5b
Deydeepya <ddpyawork@gmail.com>
2024-12-15 21:56:48 +0300

    no message

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,4 @@
 import {Navbar, Sidebar} from "../Components/compIndex";
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
-import "../Sidebar.css";
-
->>>>>>> 3873906c7a741df63353fb77d7eb314611c275bb
->>>>>>> 37ad910cc66cef04fa09882816f3d68bc4e2bfde
 
 const Home = () => {
   return (
