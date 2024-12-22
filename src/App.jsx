32773bab7a49eab8c5f33b16ced48248fe30d2d4
BHASKAR <bassnaidu1242@gmail.com>
2024-12-22 09:18:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,34 @@
-import { Routes, Route } from "react-router-dom";
-import {
-  Home,
-  Notifications,
-  Projects,
-  Reports,
-  Settings,
-  Categories,
-  Stores,
-  Navbar,
-  Signin,
-  SignUp,
-  Logout,
-  Profile, // Added Profile
-} from "./Components/pageIndex";
-import { CoreDashboard, FAQ } from "@/Components/compIndex";
-import "./App.css";
-
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Home" element={<Home />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Signin />} />
-      <Route path="/logout" element={<Logout />} />
-      <Route path="/user-profile" element={<Profile />} />
-      <Route path="/notifications" element={<Notifications />} />
-      <Route path="/reports" element={<Reports />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/stores" element={<Stores />} />
-      <Route path="/" element={<CoreDashboard />} />
-      <Route path="/faqs" element={<><Navbar /><FAQ /></>} />
-    </Routes>
-  );
-};
-
+import { Routes, Route } from "react-router-dom"; 
+import {   Home,
+        Notifications, 
+        Projects,  
+        Reports,  
+        Settings,  
+        Categories,   
+        Stores,   
+        Navbar,   
+        Signin,   
+        SignUp,   
+        Logout,
+       } from "./Components/pageIndex"; 
+import {CoreDashboard, FAQ} from "@/Components/compIndex"; 
+import "./App.css"; 
+const App = () => {   
+  return (     <Routes>       
+    <Route path="/" element={<Home />} />      
+    <Route path="/Home" element={<Home />} />      
+    <Route path="/signup" element={<SignUp />} />     
+    <Route path="/login" element={<Signin />} />       
+    <Route path="/logout" element={<Logout />} />       
+    <Route path="/user-profile" element={<profile />} />      
+    <Route path="/notifications" element={<Notifications />} />    
+    <Route path="/reports" element={<Reports />} />      
+    <Route path="/projects" element={<Projects />} />      
+    <Route path="/categories" element={<Categories />} />        
+    <Route path="/stores" element={<Stores />} />      
+    <Route path="/" element={<CoreDashboard />} />        
+    <Route path="/faqs" element={<><Navbar/><FAQ /></>} />    
+  </Routes>  
+         ); 
+};  
 export default App;
