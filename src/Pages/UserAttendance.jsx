37282af1f25f9129bcb6a5fd3fcd8fdd9de16862
Viral Vaghela <viralvaghela8577@gmail.com>
2024-12-29 14:27:24 +0530

    Fix import path casing for Card components in UserAttendance page

--- src/Pages/UserAttendance.jsx
+++ src/Pages/UserAttendance.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Navbar, SideNav } from "@/Components/compIndex";
 import axios from "axios";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/Components/ui/card";
 import {
   Calendar,
   Clock,
